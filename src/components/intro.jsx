76ff9bad2ef9d5bc936947e68bf615b03ff55827
aryankumawat423 <129871919+aryankumawat423@users.noreply.github.com>
2025-06-09 12:58:08 +0530

    Add social links component and update intro section

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,36 +1,56 @@
 import React from "react";
 
+// SocialLinks component inside the same file
+function SocialLinks({ links }) {
+    return (
+        <div className="fixed top-1/2 right-8 transform -translate-y-1/2 flex flex-col items-center gap-6 z-10">
+            {links.map((item, idx) => (
+                <a key={idx} href={item.url} target="_blank" rel="noopener noreferrer">
+                    <img
+                        src={item.img}
+                        alt={item.alt || "social"}
+                        className="w-16 h-16 rounded-full object-contain"
+                    />
+                </a>
+            ))}
+        </div>
+    );
+}
+
 export default function Intro() {
+    const socialLinksData = [
+        {
+            img: "/mdi--github.svg",
+            url: "https://github.com/aryankumawat423",
+            alt: "GitHub"
+        },
+        {
+            img: "/mdi--linkedin.svg",
+            url: "https://www.linkedin.com/in/aryan-kumawat-351877270/",
+            alt: "LinkedIn"
+        },
+        {
+            img: "/mdi--instagram.svg",
+            url: "https://www.instagram.com/aryandotkumawat/",
+            alt: "Instagram"
+        },
+        {
+            img: "/material-symbols--mail-outline.svg",
+            url: "mailto:aryankumawat423@gmail.com",
+            alt: "Gmail"
+        }
+    ];
+
     return (
         <div className="relative overflow-hidden items-center justify-center h-screen text-center">
             <img src="/Avatar.svg" alt="Avatar" className="mx-auto" />
-            
-            {/* Social Links: right side, vertically centered, equal size and spacing */}
-            <div className="fixed top-1/2 right-8 transform -translate-y-1/2 flex flex-col items-center gap-6 z-10">
-                <a href="https://github.com/aryankumawat423" target="_blank" rel="noopener noreferrer">
-                    <img src="/github-mark-white.png" alt="GitHub" className="w-16 h-16 rounded-full object-contain" />
-                </a>
-                <a href="https://www.linkedin.com/in/aryan-kumawat-351877270/" target="_blank" rel="noopener noreferrer">
-                    <img src="/glyph-logo_May2016.png" alt="LinkedIn" className="w-16 h-16 rounded-full object-contain" />
-                </a>
-                <a href="https://www.instagram.com/aryandotkumawat/" target="_blank" rel="noopener noreferrer">
-                    <img src="/vecteezy_instagram-logo-png-instagram-icon-transparent_18930692.png" alt="Instagram" className="w-16 h-16 rounded-full object-contain"/>
-                </a>
-                <a href="mailto:aryankumawat423@gmail.com">
-                    <img 
-                        src="/icons8-gmail-logo.svg" 
-                        alt="Gmail" 
-                        className="w-16 h-16 rounded-full object-contain" 
-                    />
-                </a>
-            </div>
-            
+            <SocialLinks links={socialLinksData} />
             <h1 
                 className="text-5xl font-extrabold mt-4 bg-gradient-to-r from-[#A981FE] to-[#DBA6F4] text-transparent bg-clip-text"
             >
                 Hi. I'm Aryan Kumawat. <br /> A Web Developer.
             </h1>
-            
+        
             <img src="/Frame 824.svg" alt="Frame" className="mx-auto mt-4" />
         </div>
     );
